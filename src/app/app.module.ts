eec9df240c068080e8694d694f2c783493e23967
renatamendes <t2m.remsilva@terceiros.mag.com.br>
2023-03-25 15:28:05 -0300

    Limpa a tela inicial do projeto e coloca titulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+//sempre que houver o arroba é porque possui diretivas
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //componente que faz parte desse módulo
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //adiciona os pacotes que foram imporados, do angular ou de outra lib
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    //modulo iniciado quando é executado o sistema
+  ],
 })
-export class AppModule { }
+export class AppModule {}
