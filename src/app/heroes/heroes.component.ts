fab3d9eea70c6fb6c8f165ee6de843d0bff19e0f
renatamendes <t2m.remsilva@terceiros.mag.com.br>
2023-03-25 18:16:46 -0300

    Criação da lista de heróis e uso de diretivas

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,23 @@
+import { HEROES } from './../mock/mock-heroes';
 import { Hero } from '../interfaces/hero.model';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
   //criando a primeira propriedade
   hero: Hero = {
     id: 1,
     name: 'Wolverirne',
   };
 
-  //usado para injeção de dependencia e inicialização de varivável
-  constructor() {}
+  heroes = HEROES; //listagem que possui todos os heróis
+  selectedHero?: Hero; //quando é necessário inicializar uma variável vazia, é necessário utilizar o ?, para que aceite null ou undefined
 
-  //o OnInit faz parte do ciclo de vida do component, ou seja, logo após o component ser criado ele executa tudo que estiver dentro do onInit [não retorna nada]
-  ngOnInit(): void {}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero; //this é conhecido para o contexto da classe
+  }
 }
